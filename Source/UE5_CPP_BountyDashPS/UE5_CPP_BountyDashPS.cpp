38d22a22dfbc085f38ab8d29f4d8033cd5a73a07
Katsanakis Georgios <geo.elgeo@gmail.com>
2022-08-01 07:42:50 +0200

    Initial commit

---
+++ Source/UE5_CPP_BountyDashPS/UE5_CPP_BountyDashPS.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UE5_CPP_BountyDashPS.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE5_CPP_BountyDashPS, "UE5_CPP_BountyDashPS" );
---
+++ Source/UE5_CPP_BountyDashPS/UE5_CPP_BountyDashPSGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE5_CPP_BountyDashPSGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5_CPP_BOUNTYDASHPS_API AUE5_CPP_BountyDashPSGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
