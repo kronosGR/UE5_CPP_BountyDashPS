fb7ad3d9568093d5c8b4c05ccc9d3825e672712b
Katsanakis Georgios <geo.elgeo@gmail.com>
2022-08-03 08:04:11 +0200

    started gamemode implementation

--- Source/UE5_CPP_BountyDashPS/UE5_CPP_BountyDashPSGameModeBase.cpp
+++ Source/UE5_CPP_BountyDashPS/UE5_CPP_BountyDashPSGameModeBase.cpp
@@ -3,3 +3,30 @@
 
 #include "UE5_CPP_BountyDashPSGameModeBase.h"
 
+#include "BountyDashCharacter.h"
+
+AUE5_CPP_BountyDashPSGameModeBase::AUE5_CPP_BountyDashPSGameModeBase()
+{
+	 DefaultPawnClass = ABountyDashCharacter::StaticClass();
+
+	numCoinsForSpeedIncrease = 5;
+	gameSpeed= 10.f;
+	gameSpeedIncrease = 5.0f;
+	gameLevel =1;
+}
+
+void AUE5_CPP_BountyDashPSGameModeBase::CharScoreUp(unsigned charScore)
+{
+}
+
+float AUE5_CPP_BountyDashPSGameModeBase::GetInvGameSpeed()
+{
+}
+
+float AUE5_CPP_BountyDashPSGameModeBase::GetGameSpeed()
+{
+}
+
+int32 AUE5_CPP_BountyDashPSGameModeBase::GetGameLevel()
+{
+}
--- Source/UE5_CPP_BountyDashPS/UE5_CPP_BountyDashPSGameModeBase.h
+++ Source/UE5_CPP_BountyDashPS/UE5_CPP_BountyDashPSGameModeBase.h
@@ -13,5 +13,32 @@ UCLASS()
 class UE5_CPP_BOUNTYDASHPS_API AUE5_CPP_BountyDashPSGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
+
+	UPROPERTY()
+	float gameSpeed;
+
+	UPROPERTY()
+	int32 gameLevel;
+
+public:
+	AUE5_CPP_BountyDashPSGameModeBase();
+	
+	void CharScoreUp(unsigned int charScore);
+
+	UFUNCTION()
+	float GetInvGameSpeed();
+
+	UFUNCTION()
+	float GetGameSpeed();
+
+	UFUNCTION()
+	int32 GetGameLevel();
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	int32 numCoinsForSpeedIncrease;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float gameSpeedIncrease;
 	
 };
