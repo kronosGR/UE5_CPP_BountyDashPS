19b1b63006affac87bfbb4a2a0cbd074ad5caef7
Katsanakis Georgios <geo.elgeo@gmail.com>
2022-08-07 07:36:48 +0200

    implemented costructor and beginplay

--- Source/UE5_CPP_BountyDashPS/CoinSpawner.cpp
+++ Source/UE5_CPP_BountyDashPS/CoinSpawner.cpp
@@ -3,26 +3,53 @@
 
 #include "CoinSpawner.h"
 
+#include "EngineUtils.h"
+#include "Floor.h"
+
 // Sets default values
 ACoinSpawner::ACoinSpawner()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	RootComponent = Root;
+
+	MaxSetCoins = 5;
+	MinSetCoins = 3;
+	CoinSetTimeInterval = 4.0f;
+	CoinTimeInterval = 0.5f;
+	MovementTimeInterval = 1.0f;
 }
 
 // Called when the game starts or when spawned
 void ACoinSpawner::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	for (TActorIterator<ATargetPoint> TargetIter(GetWorld()); TargetIter; ++TargetIter)
+	{
+		SpawnTransforms.Add(*TargetIter);
+	}
+
+	for (TActorIterator<AFloor> FloorIter(GetWorld()); FloorIter; ++FloorIter)
+	{
+		if (FloorIter->GetWorld() == GetWorld())
+		{
+			KillPoint = FloorIter->GetKillPoint();
+			SpawnPoint = FloorIter->GetSpawnPoint();
+		}
+	}
+
+	FTimerManager& worldTimeManager = GetWorld()->GetTimerManager();
+	worldTimeManager.SetTimer(CoinSetTimerHandle, this, &ACoinSpawner::SpawnCoinSet, CoinSetTimeInterval, false);
+	worldTimeManager.SetTimer(SpawnMoveTimerHandle, this, &ACoinSpawner::MoveSpawner, MovementTimeInterval, true);
 }
 
 // Called every frame
 void ACoinSpawner::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
 void ACoinSpawner::SpawnCoin()
@@ -36,4 +63,3 @@ void ACoinSpawner::SpawnCoinSet()
 void ACoinSpawner::MoveSpawner()
 {
 }
-
