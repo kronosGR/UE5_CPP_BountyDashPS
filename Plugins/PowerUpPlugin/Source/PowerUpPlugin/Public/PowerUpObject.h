fb91fb61cb0e4b5bb68032ccd3c8416fd4860429
Katsanakis Georgios <geo.elgeo@gmail.com>
2022-08-09 06:40:45 +0200

    added PowerUpObject class

---
+++ Plugins/PowerUpPlugin/Source/PowerUpPlugin/Public/PowerUpObject.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "PowerUpObject.generated.h"
+
+enum class EPowerUp: uint8
+{
+	SPEED = 1 UMETA(DisplayName="Speed"),
+	SMASH = 2 UMETA(DisplayName="Smash"),
+	MAGNET = 3 UMETA(DisplayName="Magner")
+};
+
+UCLASS()
+class POWERUPPLUGIN_API UPowerUpObject : public UObject
+{
+	GENERATED_BODY()
+
+	UPowerUpObject(const FObjectInitializer& ObjectInitializer);
+	
+public:
+	EPowerUp GetType();
+private:
+	EPowerUp Type;
+};
