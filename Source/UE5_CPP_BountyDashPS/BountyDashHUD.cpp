38845bb3b9edc0dbcd7a1706d7283ee5909500f3
Katsanakis Georgios <geo.elgeo@gmail.com>
2022-08-12 06:28:38 +0200

    declared members

---
+++ Source/UE5_CPP_BountyDashPS/BountyDashHUD.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BountyDashHUD.h"
+
+BountyDashHUD::BountyDashHUD()
+{
+}
+
+BountyDashHUD::~BountyDashHUD()
+{
+}
---
+++ Source/UE5_CPP_BountyDashPS/BountyDashHUD.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5_CPP_BOUNTYDASHPS_API ABountyDashHUD : public AHUD
+{
+	GENERATED_BODY()
+	ABountyDashHUD();
+
+	UPROPERTY()
+	UFont* HUDFont;
+
+	virtual void DrawHUD() override;
+};
